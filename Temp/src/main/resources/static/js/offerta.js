95596dbc5f08f6d6e77f17ba5c0278e65695619c
Administrator <Administrator@10.16.4.119>
2018-05-11 01:39:31 -0700

    terminamento match js

--- Temp/src/main/resources/static/js/offerta.js
+++ Temp/src/main/resources/static/js/offerta.js
@@ -3,7 +3,8 @@ $(document).on('click','#bottoneStato',(function(){
 	$.ajax({
 		url: "offerta/stato",
 		success: function(result){
-			
+		    $("#div1").empty();
+
 			$.each(result,function(i,item){
 				document.getElementById("div1").innerHTML += "<br><br><table style='width:40%'><tr> <th>RUOLO</th> <th>STATO OFFERTA</th> <th>DIPARTIMENTO</th> <th>COMPETENZE</th> </tr>" +
 						"<tr><td>" + item.ruolo +"</td><td>" + item.statoOfferta + "</td>" +
@@ -20,19 +21,22 @@ document.getElementById("div1").innerHTML += "</table>"
 $(document).on('click','.linkDettaglio', function(evt){
 	evt.preventDefault();
     var href = this.href;
+    $("#newDiv").empty();
+    $("#matchDiv").empty();
 	$.ajax({
 		url: href,
 		success: function(result){
 			
 			console.log(result);
 			
-			document.getElementById("div1").innerHTML += "<br><tr><td>" + result.ruolo + " " +"</td><td>" 
-			+ result.statoOfferta + " "+ "</td><td>" + result.dipartimento + " " + "</td><td>" 
+		    $("#newDiv").append("<div class='newDiv'> <br><tr><td>" + result.ruolo + " " +"</td><td>" 
+		    		+ result.statoOfferta + " "+ "</td><td>" + result.dipartimento + " " + "</td><td>" )  
+			
 			$.each(result.competenze,function(i,item){ 
-				document.getElementById("div1").innerHTML += "<tr><td>" + item.categoria + " " + item.nome +  "</td></tr> " 
+				$("#newDiv").append("<tr><td>" + item.categoria + " " + item.nome +  "</td></tr> " ) 
 				});
-			document.getElementById("div1").innerHTML += "<p>inserisci il numero di candidati desiderati:</p><input type='text' >" +
-					"</input><a class='linkMatch' href='offerta/match/"+result.idOfferta+"'>Clicca per eseguire il matching</a> <br>"
+		    $("#newDiv").append("<p>inserisci il numero di candidati desiderati:</p><input id='nPag' type='number' >" +
+					"</input><a class='linkMatch' href='offerta/matchN/"+result.idOfferta+"'>Clicca per eseguire il matching</a> <br></div>")
 		}
 	 
 		});
@@ -40,15 +44,22 @@ $(document).on('click','.linkDettaglio', function(evt){
 });
 
 $(document).on('click','.linkMatch', function(evt){
+	$("#matchDiv").empty();
+	var pag = $('#nPag').val()
+	console.log(pag);
 	evt.preventDefault();
-    var href = this.href;
-    
+    var href = this.href + "/" + pag;
     console.log(href);
 	$.ajax({
 		url: href,
 		success: function(result){
-			console.log("hello");
+			
+			 
 			console.log(result);
+			$.each(result,function(i,item){ 
+				 $("#matchDiv").append("<tr><td>" + item.candidato.nome + " " + item.candidato.cognome + " " + item.candidato.eta + " livello: "+ item.contatore +"</td></tr>")
+				
+			});
 		}
 	 
 		});
